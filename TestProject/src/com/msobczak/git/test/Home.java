0591e024a05445ed010f9090be2602e7d3ad97db
mikesobczak <msobczak@yahoo.com>
2014-02-21 17:07:57 -0500

    Revision #2.

--- TestProject/src/com/msobczak/git/test/Home.java
+++ TestProject/src/com/msobczak/git/test/Home.java
@@ -3,6 +3,7 @@ package com.msobczak.git.test;
 public class Home {
 	
 	private String msg;
+	private int number;
 
 	public String getMsg() {
 		return msg;
@@ -10,6 +11,14 @@ public class Home {
 
 	public void setMsg(String msg) {
 		this.msg = msg;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
 	}
 	
 	
