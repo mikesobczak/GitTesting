f681cff98d3a9f6432a082a655e790932c7bd776
mikesobczak <msobczak@yahoo.com>
2014-02-21 16:52:14 -0500

    Revised.

--- TestProject/src/com/msobczak/git/test/Home.java
+++ TestProject/src/com/msobczak/git/test/Home.java
@@ -1,5 +1,17 @@
 package com.msobczak.git.test;
 
 public class Home {
+	
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
 
 }
